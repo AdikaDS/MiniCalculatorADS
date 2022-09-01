34c419a39e63e737bded069ae591ed7bbb01f124
AdikaDS <adika.dwi.saputra@mail.ugm.ac.id>
2022-09-02 00:17:05 +0700

    initial commit

---
+++ app/src/main/java/com/example/minicalculatorads/VersiConstraintActivity.java
@@ -0,0 +1,14 @@
+package com.example.minicalculatorads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VersiConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.versi_constraint);
+    }
+}
---
+++ app/src/main/java/com/example/minicalculatorads/VersiRelativeActivity.java
@@ -0,0 +1,14 @@
+package com.example.minicalculatorads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VersiRelativeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.versi_relative);
+    }
+}
